5fadd3297117350f2e49bdba6b4b51a795aff74e
ElrohirGT <elrohirgt@gmail.com>
2024-03-23 22:07:23 -0600

    feat: Added basic instructions view

--- ExploraXAssement/components/pages/Instrucctions.js
+++ ExploraXAssement/components/pages/Instrucctions.js
@@ -4,7 +4,13 @@ import {
   useWindowDimensions,
   Pressable,
   Text,
+  Image,
 } from "react-native";
+
+const avatar = require("../../assets/elementos_estaticos/Chanín.png");
+const dividerTitulo = require("../../assets/elementos_estaticos/Línea_título.png");
+const dGlow = require("../../assets/elementos_estaticos/pantalla_puntos/DconGlow.png");
+const dGlowFire = require("../../assets/elementos_estaticos/pantalla_puntos/Llama.png");
 
 export default function Instructions({
   title,
@@ -16,9 +22,28 @@ export default function Instructions({
   const styles = StyleSheet.flatten(mobileStyles, webStyles);
   const { height, width } = useWindowDimensions();
   const contentWidth = (width * 3) / 4;
+  const dGlowDimension = contentWidth / 4;
 
   return (
     <View style={styles.container} {...props}>
+      <View style={styles.dGlowContainer}>
+        <Image
+          source={dGlowFire}
+          style={[
+            styles.dGlowFire,
+            { width: dGlowDimension, height: dGlowDimension - 5 },
+          ]}
+          resizeMode="contain"
+        />
+        <Image
+          source={dGlow}
+          style={[
+            styles.dGlowLogo,
+            { width: dGlowDimension, height: dGlowDimension },
+          ]}
+          resizeMode="contain"
+        />
+      </View>
       <View
         style={[
           styles.mainContent,
@@ -26,13 +51,21 @@ export default function Instructions({
         ]}
       >
         <Text style={styles.title}>{title}</Text>
-        <View style={[styles.divider, { width: contentWidth / 2 }]} />
+        <Image
+          source={dividerTitulo}
+          style={[styles.divider, { width: contentWidth / 2 }]}
+        />
         <Text style={styles.description}>{description}</Text>
         <Text style={styles.mission}>{mission}</Text>
         <Pressable style={styles.button}>
           <Text style={styles.buttonText}>¡ACEPTO EL RETO!</Text>
         </Pressable>
       </View>
+      <Image
+        source={avatar}
+        resizeMode="contain"
+        style={[styles.chaninImage, { height: height / 3 }]}
+      />
     </View>
   );
 }
@@ -41,7 +74,26 @@ const mobileStyles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    transform: [{ translateY: 50 }],
   },
+
+  dGlowContainer: {
+    flexDirection: "row",
+    transform: [{ scale: 1.6 }, { translateX: 75 }, { translateY: 40 }],
+    zIndex: 2,
+  },
+
+  dGlowFire: {},
+
+  dGlowLogo: {
+    transform: [{ translateY: -6 }, { translateX: -15 }],
+  },
+
+  chaninImage: {
+    alignSelf: "flex-start",
+    width: 100,
+  },
+
   mainContent: {
     gap: 5,
     padding: 30,
@@ -50,23 +102,27 @@ const mobileStyles = StyleSheet.create({
     backgroundColor: "#1f4c8b",
     borderBottomWidth: 4,
     borderBottomColor: "#132f51",
+    transform: [{ translateY: 50 }],
   },
+
   divider: {
-    backgroundColor: "#ae73d2",
     alignSelf: "center",
     height: 3,
   },
+
   title: {
     color: "white",
     fontSize: 40,
     fontWeight: "bold",
     textAlign: "center",
   },
+
   description: {
     fontSize: 20,
     color: "white",
     textAlign: "center",
   },
+
   mission: {
     fontSize: 20,
     fontWeight: "bold",
@@ -74,6 +130,7 @@ const mobileStyles = StyleSheet.create({
     color: "white",
     paddingBottom: 20,
   },
+
   button: {
     paddingTop: 5,
     paddingBottom: 5,
@@ -85,6 +142,7 @@ const mobileStyles = StyleSheet.create({
     borderBottomWidth: 4,
     borderBottomColor: "#8d8d8d",
   },
+
   buttonText: {
     fontSize: 20,
     textAlign: "center",
