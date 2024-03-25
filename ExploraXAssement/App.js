b2963aee4ea9f1aba6bd485396d102ac48b5078a
ElrohirGT <elrohirgt@gmail.com>
2024-03-25 09:45:48 -0600

    fix: Header website styles

--- ExploraXAssement/App.js
+++ ExploraXAssement/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
-import { DEFAULT_QUESTIONS } from "./constants";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -8,11 +7,10 @@ import Footer from "./components/Footer";
 const background = require("./assets/elementos_estaticos/Fondo.png");
 
 export default function App() {
-  const [questions, setQuestions] = useState(DEFAULT_QUESTIONS);
   const [money, setMoney] = useState(0);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <ImageBackground
         source={background}
         style={styles.backgroundImage}
@@ -22,7 +20,7 @@ export default function App() {
         <Main />
         <Footer />
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 }
 
--- ExploraXAssement/components/Header.js
+++ ExploraXAssement/components/Header.js
@@ -1,4 +1,11 @@
-import { View, Text, Pressable, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  Image,
+  useWindowDimensions,
+} from "react-native";
 import { IS_ANDROID, IS_WEB } from "../constants";
 import UpArrowSVG from "./atoms/UpArrowSVG";
 
@@ -6,16 +13,19 @@ const coin = require("../assets/elementos_estaticos/moneda.png");
 const planet = require("../assets/elementos_estaticos/PlanetaAritmética.png");
 
 export default function Header({ money }) {
-  const styles = IS_WEB
-    ? StyleSheet.flatten(mobileStyles, webStyles)
-    : mobileStyles;
+  const { width, height } = useWindowDimensions();
+  const styles = generateStyles(width, height);
 
   return (
     <View style={styles.container}>
       <Image source={planet} resizeMode="contain" style={styles.planetImage} />
-      <View style={styles.coinDisplayContainer}>
-        <Image source={coin} style={styles.coinImage} resizeMode="contain" />
-        <Text style={styles.coinsText}>{money}</Text>
+
+      <View style={styles.coinSideContainer}>
+        <View style={styles.coinDisplayContainer}>
+          <Image source={coin} style={styles.coinImage} resizeMode="contain" />
+          <Text style={styles.coinsText}>{money}</Text>
+        </View>
+
         <Pressable style={styles.moreCoinsButton}>
           <UpArrowSVG style={styles.moreCoinsButtonIcon} />
         </Pressable>
@@ -24,62 +34,91 @@ export default function Header({ money }) {
   );
 }
 
-const mobileStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-  },
+/**
+ * Generates the styles of the component with the given screen width and screen height
+ * @param {number} screenWidth
+ * @param {number} screenHeight
+ */
+function generateStyles(screenWidth, screenHeight) {
+  const buttonWidth = screenWidth / 8;
+  const coinTextWidth = screenWidth / 5;
 
-  coinDisplayContainer: {
-    justifyContent: "flex-end",
-    flexDirection: "row",
-    alignItems: "flex-start",
-    gap: 5,
-  },
+  const mobileStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: screenWidth / 20,
+    },
 
-  coinImage: {
-    marginRight: -20,
-    zIndex: 2,
-    height: 32,
-  },
+    coinSideContainer: {
+      flexDirection: "row",
+      gap: screenWidth / 20,
+    },
 
-  planetImage: {
-    width: 64,
-  },
+    planetImage: {
+      width: screenWidth / 5,
+      height: screenHeight / 5,
+    },
 
-  coinsText: {
-    backgroundColor: "#111e55",
-    fontSize: 18,
-    minWidth: 75,
-    color: "white",
-    paddingLeft: 10,
-    borderTopRightRadius: 100,
-    borderBottomRightRadius: 100,
-    zIndex: 1,
-  },
-  moreCoinsButton: {
-    padding: 10,
-    backgroundColor: "#ec8a34",
-    width: 50,
-    height: 50,
-    borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#e85729",
-    borderBottomWidth: 4,
-  },
-  moreCoinsButtonIcon: {
-    width: 32,
-    height: 32,
-  },
-});
+    coinDisplayContainer: {
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    },
 
-const webStyles = StyleSheet.create({
-  coinImage: {
-    width: 50,
-    height: 50,
-  },
-});
+    coinImage: {
+      position: "absolute",
+      zIndex: 2,
+
+      width: screenWidth / 10,
+      height: screenHeight / 20,
+      left: screenWidth / -14,
+    },
+
+    coinsText: {
+      backgroundColor: "#111e55",
+      color: "white",
+      zIndex: 1,
+
+      minWidth: coinTextWidth,
+      fontSize: screenHeight / 45,
+      paddingLeft: coinTextWidth / 5,
+      borderTopRightRadius: screenWidth,
+      borderBottomRightRadius: screenWidth,
+    },
+
+    moreCoinsButton: {
+      backgroundColor: "#ec8a34",
+      borderRadius: 10,
+      justifyContent: "center",
+      alignItems: "center",
+      borderColor: "#e85729",
+      borderBottomWidth: 4,
+
+      width: buttonWidth,
+      height: buttonWidth,
+      maxWidth: 50,
+      maxHeight: 50,
+    },
+
+    moreCoinsButtonIcon: {
+      width: (buttonWidth * 3) / 4,
+      height: (buttonWidth * 3) / 4,
+    },
+  });
+
+  const webStyles = StyleSheet.create({
+    coinImage: {
+      position: "absolute",
+      zIndex: 2,
+
+      width: screenWidth / 10,
+      height: screenHeight / 20,
+      left: screenWidth / -20,
+    },
+  });
+
+  return StyleSheet.flatten(mobileStyles, webStyles);
+}
