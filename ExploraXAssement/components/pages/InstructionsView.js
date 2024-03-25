d819951475a9b0b79018d4a700d91cd60a8efffd
ElrohirGT <elrohirgt@gmail.com>
2024-03-25 10:51:29 -0600

    fix: Initial refactor of instructions styles

--- ExploraXAssement/components/pages/InstructionsView.js
+++ ExploraXAssement/components/pages/InstructionsView.js
@@ -19,135 +19,140 @@ export default function InstructionsView({
   onNextButtonPressed,
   ...props
 }) {
-  const styles = StyleSheet.flatten(mobileStyles, webStyles);
   const { height, width } = useWindowDimensions();
-  const contentWidth = (width * 3) / 4;
-  const dGlowDimension = contentWidth / 4;
+  const styles = generateStyles(width, height);
 
   return (
     <View style={styles.container} {...props}>
       <View style={styles.dGlowContainer}>
         <Image
           source={dGlowFire}
-          style={[
-            styles.dGlowFire,
-            { width: dGlowDimension, height: dGlowDimension - 5 },
-          ]}
+          style={styles.dGlowFire}
           resizeMode="contain"
         />
-        <Image
-          source={dGlow}
-          style={[
-            styles.dGlowLogo,
-            { width: dGlowDimension, height: dGlowDimension },
-          ]}
-          resizeMode="contain"
-        />
+        <Image source={dGlow} style={styles.dGlowLogo} resizeMode="contain" />
       </View>
-      <View
-        style={[
-          styles.mainContent,
-          { width: contentWidth, height: contentWidth },
-        ]}
-      >
+
+      <View style={styles.mainContent}>
         <Text style={styles.title}>{title}</Text>
         <Image
           source={dividerTitulo}
-          style={[styles.divider, { width: contentWidth }]}
+          style={styles.divider}
           resizeMode="contain"
         />
+
         <Text style={styles.description}>{description}</Text>
         <Text style={styles.mission}>{mission}</Text>
+
         <Pressable style={styles.button} onPress={onNextButtonPressed}>
           <Text style={styles.buttonText}>¡ACEPTO EL RETO!</Text>
         </Pressable>
       </View>
-      <Image
-        source={avatar}
-        resizeMode="contain"
-        style={[styles.chaninImage, { height: height / 3 }]}
-      />
+
+      <Image source={avatar} resizeMode="contain" style={styles.chaninImage} />
     </View>
   );
 }
 
-const mobileStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    transform: [{ translateY: 50 }],
-  },
+/**
+ * Generates the styles of the component with the given screen width and screen height
+ * @param {number} screenWidth
+ * @param {number} screenHeight
+ */
+function generateStyles(screenWidth, screenHeight) {
+  const contentWidth = (screenWidth * 3) / 4;
+  const dGlowDimension = contentWidth / 4;
 
-  dGlowContainer: {
-    flexDirection: "row",
-    transform: [{ scale: 1.6 }, { translateX: 75 }, { translateY: 40 }],
-    zIndex: 2,
-  },
+  const mobileStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      transform: [{ translateY: 50 }],
+    },
 
-  dGlowFire: {},
+    dGlowContainer: {
+      flexDirection: "row",
+      transform: [{ scale: 1.6 }, { translateX: 75 }, { translateY: 40 }],
+      zIndex: 2,
+    },
 
-  dGlowLogo: {
-    transform: [{ translateY: -6 }, { translateX: -15 }],
-  },
+    dGlowFire: {
+      width: dGlowDimension,
+      height: dGlowDimension - 5,
+    },
 
-  chaninImage: {
-    alignSelf: "flex-start",
-    width: 100,
-  },
+    dGlowLogo: {
+      width: dGlowDimension,
+      height: dGlowDimension,
+      transform: [{ translateY: -6 }, { translateX: -15 }],
+    },
 
-  mainContent: {
-    gap: 5,
-    padding: 30,
-    borderRadius: 10,
-    alignSelf: "center",
-    backgroundColor: "#1f4c8b",
-    borderBottomWidth: 4,
-    borderBottomColor: "#132f51",
-    transform: [{ translateY: 50 }],
-  },
+    chaninImage: {
+      alignSelf: "flex-start",
+      width: 100,
+      height: screenHeight / 3,
+    },
 
-  divider: {
-    alignSelf: "center",
-  },
+    mainContent: {
+      width: contentWidth,
+      height: contentWidth,
 
-  title: {
-    color: "white",
-    fontSize: 40,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+      gap: 5,
+      padding: 30,
+      borderRadius: 10,
+      alignSelf: "center",
+      backgroundColor: "#1f4c8b",
+      borderBottomWidth: 4,
+      borderBottomColor: "#132f51",
+      transform: [{ translateY: 50 }],
+    },
 
-  description: {
-    fontSize: 20,
-    color: "white",
-    textAlign: "center",
-  },
+    divider: {
+      alignSelf: "center",
+      width: contentWidth,
+    },
 
-  mission: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "white",
-    paddingBottom: 20,
-  },
+    title: {
+      color: "white",
+      fontSize: 40,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
 
-  button: {
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingRight: 10,
-    paddingLeft: 10,
-    backgroundColor: "white",
-    alignSelf: "center",
-    borderRadius: 5,
-    borderBottomWidth: 4,
-    borderBottomColor: "#8d8d8d",
-  },
+    description: {
+      fontSize: 20,
+      color: "white",
+      textAlign: "center",
+    },
 
-  buttonText: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "#133362",
-    fontWeight: "600",
-  },
-});
-const webStyles = StyleSheet.create({});
+    mission: {
+      fontSize: 20,
+      fontWeight: "bold",
+      textAlign: "center",
+      color: "white",
+      paddingBottom: 20,
+    },
+
+    button: {
+      paddingTop: 5,
+      paddingBottom: 5,
+      paddingRight: 10,
+      paddingLeft: 10,
+      backgroundColor: "white",
+      alignSelf: "center",
+      borderRadius: 5,
+      borderBottomWidth: 4,
+      borderBottomColor: "#8d8d8d",
+    },
+
+    buttonText: {
+      fontSize: 20,
+      textAlign: "center",
+      color: "#133362",
+      fontWeight: "600",
+    },
+  });
+  const webStyles = StyleSheet.create({});
+
+  return StyleSheet.flatten(mobileStyles, webStyles);
+}
