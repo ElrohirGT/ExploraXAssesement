4a2ae0ca0dd37bc41183fdabb828ecc86aab3816
ElrohirGT <elrohirgt@gmail.com>
2024-03-25 10:30:42 -0600

    fix: Style refactoring for footer and web support

--- ExploraXAssement/components/Footer.js
+++ ExploraXAssement/components/Footer.js
@@ -1,106 +1,120 @@
 import {
-  View,
-  StyleSheet,
-  Text,
   Image,
   Pressable,
+  StyleSheet,
+  Text,
   useWindowDimensions,
+  View,
 } from "react-native";
 
-import HomeSVG from "./atoms/HomeSVG";
-import StoreSVG from "./atoms/StoreSVG";
 import ChestSVG from "./atoms/ChestSVG";
+import HomeSVG from "./atoms/HomeSVG";
 import RankingSVG from "./atoms/RankingSVG";
-
-const MENU_BUTTON_DIMENSIONS = {
-  width: 50,
-  height: 50,
-};
+import StoreSVG from "./atoms/StoreSVG";
 
 const logo = require("../assets/elementos_estaticos/logo_fondo_oscuro.png");
 
 export default function Footer() {
-  const styles = StyleSheet.flatten(mobileStyles, webStyles);
   const { height, width } = useWindowDimensions();
-  const logoDimensions = {
-    width: height / 6,
-    height: height / 6,
-  };
-  const backgroundShapeHeigth = height / 6;
-  const backgroundShape = {
-    width,
-    height: width,
-    marginTop: -backgroundShapeHeigth,
-    borderRadius: width,
-    transform: [{ scaleX: 1.5 }],
-  };
+  const styles = generateStyles(width, height);
 
   return (
     <View style={[styles.container]}>
-      <View style={[styles.menu, { width }]}>
+      <View style={styles.menu}>
         <Pressable style={styles.menuButton}>
           <HomeSVG style={styles.menuButtonIcon} />
         </Pressable>
         <Pressable style={styles.menuButton}>
           <StoreSVG style={styles.menuButtonIcon} />
         </Pressable>
-        <Image
-          source={logo}
-          style={[styles.menuLogo, logoDimensions]}
-          resizeMode="contain"
-        />
+
+        <Image source={logo} style={styles.menuLogo} resizeMode="contain" />
+
         <Pressable style={styles.menuButton}>
           <ChestSVG style={styles.menuButtonIcon} />
         </Pressable>
         <Pressable style={styles.menuButton}>
           <RankingSVG styles={styles.menuButtonIcon} />
         </Pressable>
+        <View></View>
       </View>
-      <View style={[styles.backgroundShape, backgroundShape]} />
+      <View style={styles.backgroundShape} />
     </View>
   );
 }
 
-const mobileStyles = StyleSheet.create({
-  container: {
-    flex: 2,
-    alignItems: "center",
-  },
+/**
+ * Generates the styles for this component according to some screen width and screen height
+ * @param {number} screenWidth
+ * @param {number} screenHeight
+ */
+function generateStyles(screenWidth, screenHeight) {
+  const menuLogoHeight = screenHeight / 5;
 
-  backgroundShape: {
-    borderColor: "#61c9d2",
-    borderTopWidth: 5,
-    borderRightWidth: 5,
-    borderLeftWidth: 5,
-    backgroundColor: "#0e4c71",
-    zIndex: 1,
-  },
+  const mobileStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+    },
 
-  menu: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "flex-end",
-    zIndex: 2,
-    paddingRight: 20,
-    paddingLeft: 20,
-  },
+    backgroundShape: {
+      position: "absolute",
 
-  menuLogo: {},
+      borderColor: "#61c9d2",
+      borderTopWidth: 5,
+      borderRightWidth: 5,
+      borderLeftWidth: 5,
+      backgroundColor: "#0e4c71",
+      zIndex: 1,
 
-  menuButton: {
-    marginBottom: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    borderColor: "#54a0c4",
-    borderBottomWidth: 4,
-    backgroundColor: "#9bdafa",
-    ...MENU_BUTTON_DIMENSIONS,
-  },
+      width: screenWidth,
+      height: screenWidth,
+      top: menuLogoHeight / -3.5,
+      borderRadius: screenWidth,
+      transform: [{ scaleX: 1.5 }],
+    },
 
-  menuButtonIcon: {
-    ...MENU_BUTTON_DIMENSIONS,
-    width: MENU_BUTTON_DIMENSIONS.width - 10,
-  },
-});
-const webStyles = StyleSheet.create({});
+    menu: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "flex-end",
+      zIndex: 2,
+      paddingRight: 10,
+      paddingLeft: 10,
+      paddingBottom: 10,
+      gap: screenWidth / 50,
+
+      width: screenWidth,
+    },
+
+    menuLogo: {
+      flex: 3,
+      alignSelf: "center",
+      height: menuLogoHeight,
+      marginBottom: menuLogoHeight / 5,
+    },
+
+    menuButton: {
+      flex: 1,
+      alignSelf: "flex-end",
+      alignItems: "center",
+      justifyContent: "center",
+      height: menuLogoHeight / 4,
+
+      borderRadius: 5,
+      borderColor: "#54a0c4",
+      borderBottomWidth: 4,
+      backgroundColor: "#9bdafa",
+    },
+
+    menuButtonIcon: {
+      width: screenWidth / 8,
+      height: menuLogoHeight / 5,
+    },
+  });
+
+  const webStyles = StyleSheet.create({});
+
+  return StyleSheet.flatten(mobileStyles, webStyles);
+}
